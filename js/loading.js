1dfdeb446403f87144bb3e1a53ebf9fa1fe2d61a
Lucas <lucasaugustosuarez@hotmail.com>
2022-10-06 00:20:09 -0300

    Preentrega 3

---
+++ js/loading.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    document.querySelector('.spinner-style').classList.toggle('loading');
+})
+
--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ const up = document.querySelector('.fa-chevron-up');
 window.addEventListener('scroll', () => {
     window.scrollY > 300 ? up.classList.add('active') : up.classList.remove('active')
 })
+
 up.addEventListener('click', () => window.scrollTo(0,0))
